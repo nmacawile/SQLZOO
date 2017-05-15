c393df48084fa45a795e126dcca60508bb96676c
nmacawile <nmacawile@gmail.com>
2017-05-15 11:26:57 +0800

    Align 09_Self_JOIN spacing

--- 09_Self_JOIN.sql
+++ 09_Self_JOIN.sql
@@ -37,22 +37,20 @@ Execute the self join shown and observe that b.stop gives all the places you can
 */
 
 SELECT a.company, a.num, a.stop, b.stop
-FROM route a JOIN route b
-             ON (a.company = b.company AND a.num = b.num)
-     JOIN stops stopa ON a.stop = stopa.id
-     JOIN stops stopb ON b.stop = stopb.id
-WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'London Road';
+  FROM route a JOIN route b ON (a.company = b.company AND a.num = b.num)
+       JOIN stops stopa ON a.stop = stopa.id
+       JOIN stops stopb ON b.stop = stopb.id
+ WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'London Road';
 
 /* 6
 The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown. If you are tired of these places try 'Fairmilehead' against 'Tollcross'
 */
 
 SELECT a.company, a.num, stopa.name, stopb.name
-FROM route a JOIN route b
-             ON (a.company = b.company AND a.num = b.num)
-     JOIN stops stopa ON a.stop = stopa.id
-     JOIN stops stopb ON b.stop = stopb.id
-WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'London Road';
+  FROM route a JOIN route b ON (a.company = b.company AND a.num = b.num)
+       JOIN stops stopa ON a.stop = stopa.id
+       JOIN stops stopb ON b.stop = stopb.id
+ WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'London Road';
 
 /* 7
 Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
@@ -68,22 +66,20 @@ Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcro
 */
 
 SELECT a.company, a.num
-FROM route a JOIN route b
-             ON (a.company = b.company AND a.num = b.num)
-     JOIN stops stopa ON a.stop = stopa.id
-     JOIN stops stopb ON b.stop = stopb.id
-WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross';
+  FROM route a JOIN route b ON (a.company = b.company AND a.num = b.num)
+       JOIN stops stopa ON a.stop = stopa.id
+       JOIN stops stopb ON b.stop = stopb.id
+ WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross';
 
 /* 9
 Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. Include the company and bus no. of the relevant services.
 */
 
 SELECT DISTINCT stopa.name, a.company, a.num
-FROM route a JOIN route b
-             ON (a.company = b.company AND a.num = b.num)
-     JOIN stops stopa ON a.stop = stopa.id
-     JOIN stops stopb ON b.stop = stopb.id
-WHERE stopb.name = 'Craiglockhart';
+  FROM route a JOIN route b ON (a.company = b.company AND a.num = b.num)
+       JOIN stops stopa ON a.stop = stopa.id
+       JOIN stops stopb ON b.stop = stopb.id
+ WHERE stopb.name = 'Craiglockhart';
 
 /* 10
 Find the routes involving two buses that can go from Craiglockhart to Sighthill.
